43d1cd5814407ea33360c7b25a783e799c6973c6
nikzayn <nikhilvaidyar1997@gmail.com>
2023-02-21 20:16:06 +0530

    naming convention update for existing parking model

--- parking-lot/models/ParkingSchema.go
+++ parking-lot/models/ParkingSchema.go
@@ -1,30 +1,30 @@
-package ParkingLot
+package models
 
 import "time"
 
 //Define a Parking lot with levels embedded
 type ParkingLot struct {
-	levels []*Level
+	Levels []*Level
 }
 
 //Define a Level with Parking spot embedded
 type Level struct {
-	spots []*ParkingSpot
+	Spots []*ParkingSpot
 }
 
 //Define a Parking Spot according to size, availability and vehicle details
 type ParkingSpot struct {
-	id          int
-	size        int
-	isAvailable bool
-	vehicle     *Vehicle
+	Id          int
+	Size        int
+	IsAvailable bool
+	Vehicle     *Vehicle
 }
 
 //Define a vehicle size, entryTime and exitTime
 type Vehicle struct {
-	size      VehicleSize
-	entryTime time.Time
-	exitTime  time.Time
+	Size      VehicleSize
+	EntryTime time.Time
+	ExitTime  time.Time
 }
 
 //Define a vehicle size
@@ -32,6 +32,6 @@ type VehicleSize int
 
 const (
 	Motorcycle VehicleSize = iota
-	Car
-	Truck
+	Compact
+	Large
 )
