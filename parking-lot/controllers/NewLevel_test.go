5887401d17b0eb7f8f21ecfe875a75ff649a85ac
nikzayn <nikhilvaidyar1997@gmail.com>
2023-02-25 22:58:46 +0530

    test case initial --commit

---
+++ parking-lot/controllers/NewLevel_test.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	models "github.com/nikzayn/golang-lld/parking-lot/models"
+)
+
+type NewLevelTest struct {
+	arg1     int
+	arg2     int
+	expected models.Level
+}
+
+// func TestNewLevel(t *testing.T) {
+// 	testCases := []NewLevelTest{
+// 		{
+// 			arg1: 3,
+
+// 		}
+// 	}
+// }
