e6d9fb1e4b48e5c7f04efbf05b357bf3d8301a49
nikzayn <nikhilvaidyar1997@gmail.com>
2023-02-26 23:22:06 +0530

    added launch.json and created parking lot and levels with dummy data

--- parking-lot/main.go
+++ parking-lot/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/nikzayn/golang-lld/parking-lot/controllers"
+)
+
+func main() {
+	numberOfLevels := 5
+	spotsPerLevel := 10
+
+	//Created a parking lot with number of levels and number of spot per level
+	generateParkingLot := controllers.NewParkingLot(numberOfLevels, spotsPerLevel)
+	generateLevels := controllers.NewLevel(spotsPerLevel, 2)
+
+	fmt.Printf("%v %v", generateParkingLot, *generateLevels)
+}
