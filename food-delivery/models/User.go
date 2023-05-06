0ae70bfbb672f7483d92647e18c4687050cbb95e
nikzayn <nikhilvaidyar1997@gmail.com>
2023-05-06 22:05:54 +0530

    user create method --added

--- food-delivery/models/User.go
+++ food-delivery/models/User.go
@@ -10,3 +10,13 @@ type User struct {
 	ContactNumber string
 	Pincode       string
 }
+
+func (u *User) createUser() *User {
+	user := User{
+		Name:          "Nikhil",
+		Gender:        "Male",
+		ContactNumber: "7042224751",
+		Pincode:       "110063",
+	}
+	return &user
+}
